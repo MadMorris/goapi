9091b9719958108d7c7bea172ca2e57700248509
Mauricio Vega <mevega79@gmail.com>
2018-04-12 23:50:01 -0300

    Updated task by ID endpoint

--- handler.task.go
+++ handler.task.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/gorilla/mux"
 )
@@ -14,7 +15,7 @@ var collection = session.DB(getTodoDBName()).C("tasks")
 // TaskList : route to task list
 func TaskList(w http.ResponseWriter, r *http.Request) {
 	var res Tasks
-	err := collection.Find(nil).All(&res)
+	err := collection.Find(nil).Sort("-_id").All(&res)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -54,5 +55,23 @@ func TaskAdd(w http.ResponseWriter, r *http.Request) {
 func TaskByID(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	taskID := params["id"]
-	fmt.Fprintf(w, "Task by ID %s", taskID)
+
+	if !bson.IsObjectIdHex(taskID) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid := bson.ObjectIdHex(taskID)
+
+	taskData := Task{}
+	err := collection.FindId(oid).One(&taskData)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(taskData)
 }
